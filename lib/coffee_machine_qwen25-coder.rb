cb574f7cd6bfc91ccddfe8daa5d9d1dae9969310
Alan Ridlehoover <alan@ridlehoover.com>
2025-02-23 18:28:06 -0800

    Updated Qwen25-coder with largest model available via Ollama.
    Passed all but one test (the one about serving hot water).

--- lib/coffee_machine_qwen25-coder.rb
+++ lib/coffee_machine_qwen25-coder.rb
@@ -1,50 +1,55 @@
 class CoffeeMachine
-  def initialize(driver: Driver.new)
+  def initialize(driver:)
     @driver = driver
   end
 
   def vend(drink: :coffee, options: {})
+    @driver.dispense_cup
+    @driver.heat_water
+
     case drink
-    when :coffee
-      serve_coffee(options)
     when :tea
       serve_tea(options)
+    when :cocoa
+      serve_cocoa(options)
     else
-      serve_unknown_drink
+      serve_coffee(options)
     end
   end
 
   private
 
-  def serve_coffee(options)
-    @driver.dispense_cup
-    @driver.heat_water
+  def serve_coffee(options = {})
     @driver.prepare_grounds
     @driver.dispense_water
+    add_sweetener(options) if options[:sweet]
+    add_cream(options) if options[:creamy]
+    add_whipped_cream(options) if options[:fluffy]
     @driver.dispose_of_grounds
+  end
+
+  def serve_tea(options = {})
+    @driver.dispense_tea_bag
+    @driver.dispense_water
     add_sweetener(options) if options[:sweet]
     add_cream(options) if options[:creamy]
   end
 
-  def serve_tea(options)
-    @driver.dispense_cup
-    @driver.heat_water
-    @driver.dispense_tea_bag
+  def serve_cocoa(options = {})
+    @driver.dispense_cocoa_mix
     @driver.dispense_water
-    add_sweetener(options) if options[:sweet]
+    add_whipped_cream(options) if options[:fluffy]
   end
 
-  def serve_unknown_drink
-    @driver.dispense_cup
-    @driver.heat_water
-    @driver.dispense_water
+  def add_sweetener(_options)
+    @driver.dispense_sweetener
   end
 
-  def add_sweetener(options)
-    @driver.dispense_sweetener unless options[:sweet].nil?
+  def add_cream(_options)
+    @driver.dispense_cream
   end
 
-  def add_cream(options)
-    @driver.dispense_cream unless options[:creamy].nil?
+  def add_whipped_cream(_options)
+    @driver.dispense_whipped_cream
   end
 end
